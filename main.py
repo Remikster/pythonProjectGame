be043d550ea46964108c1eeb50981c2c0fba43b5
Матвей <55951185+Remikster@users.noreply.github.com>
2022-02-05 12:29:54 +0300

    Add enemy

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("0")
 clock = pygame.time.Clock()
+sprites = pygame.sprite.Group()
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -45,10 +46,36 @@ class Player(pygame.sprite.Sprite):
         if self.rect.left < 0:
             self.rect.left = 0
 
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30, 30))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -40)
+        self.speedy = random.randrange(1, 8)
+        self.speedx = random.randrange(-3, 3)
+
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(1, 8)
 
 all_sprites = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
+all_sprites = sprites
+enemy = sprites
+player = Player()
+all_sprites.add(player)
+for i in range(8):
+    en = Enemy()
+    all_sprites.add(en)
+    enemy.add(en)
 
 running = True
 while running:

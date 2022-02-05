9c5b02199c572ab40fdd47ae98d31d55af41c67c
Матвей <55951185+Remikster@users.noreply.github.com>
2022-02-05 11:50:15 +0300

    Player Moving

--- main.py
+++ main.py
@@ -15,8 +15,40 @@ YELLOW = (255, 255, 0)
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("My Game")
+pygame.display.set_caption("0")
 clock = pygame.time.Clock()
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30, 30))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+
+    def update(self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -10
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 10
+        if keystate[pygame.K_DOWN]:
+            self.speedy = -10
+        if keystate[pygame.KEYUP]:
+            self.speedy = 10
+        self.rect.x += self.speedx
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 
 running = True
 while running:
@@ -25,8 +57,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    all_sprites.update()
 
     screen.fill(BLACK)
-    pygame.display.flip()
+    all_sprites.draw(screen)
+    pygame.display.update()
 
 pygame.quit()

eac5b1d9ea7f902b34de1d8ff3d867f7bdc7c321
Матвей <55951185+Remikster@users.noreply.github.com>
2022-02-05 11:24:24 +0300

    add window

---
+++ main.py
@@ -0,0 +1,32 @@
+import pygame
+import random
+
+WIDTH = 600
+HEIGHT = 800
+FPS = 60
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    screen.fill(BLACK)
+    pygame.display.flip()
+
+pygame.quit()

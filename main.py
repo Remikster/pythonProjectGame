a7f8edcea21fe5ac7c85624ae7bafab409fe0c32
Матвей <55951185+Remikster@users.noreply.github.com>
2022-02-12 16:59:06 +0300

    add circle collider and background( temporary)

--- main.py
+++ main.py
@@ -21,12 +21,23 @@ clock = pygame.time.Clock()
 game_fold = path.dirname(__file__)
 img_folder = path.join(game_fold, 'img')
 
+font_name = pygame.font.match_font('arial')
+
+
+def draw_text(surf, text, size, x, y):
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, WHITE)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surface, text_rect)
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = player_img
         self.rect = self.image.get_rect()
+        self.radius = 25
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
         self.speedx = 0
@@ -71,6 +82,7 @@ class Enemy(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = enemy_img
         self.rect = self.image.get_rect()
+        self.radius = int(self.rect.width * .85 / 2)
         self.rect.x = random.randrange(WIDTH - self.rect.width)
         self.rect.y = random.randrange(-100, -40)
         self.speedy = random.randrange(1, 8)
@@ -85,6 +97,28 @@ class Enemy(pygame.sprite.Sprite):
             self.speedy = random.randrange(1, 8)
 
 
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = enemy_img
+        self.rect = self.image.get_rect()
+        self.radius = int(self.rect.width * .85 / 2)
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -40)
+        self.speedy = random.randrange(1, 8)
+        self.speedx = random.randrange(-3, 3)
+
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(1, 8)
+
+
+background = pygame.image.load(path.join(img_folder, 'fon.png'))
+background_rect = background.get_rect()
 player_img = pygame.image.load(path.join(img_folder, 'w-01-150.png'))
 enemy_img = pygame.image.load(path.join(img_folder, 'Ghost.png'))
 
@@ -113,16 +147,19 @@ while running:
 
     hits = pygame.sprite.groupcollide(enemy, bullets, True, True)
     for hit in hits:
+        score += 10
         en = Enemy()
         all_sprites.add(en)
         enemy.add(en)
 
-    hits = pygame.sprite.spritecollide(player, enemy, False)
+    hits = pygame.sprite.spritecollide(player, enemy, False, pygame.sprite.collide_circle)
     if hits:
         running = False
 
     screen.fill(BLACK)
+    screen.blit(background, background_rect)
     all_sprites.draw(screen)
+    draw_text(screen, str(score), 18, WIDTH / 2, 10 )
     pygame.display.update()
 
 
